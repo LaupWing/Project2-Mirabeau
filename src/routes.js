98f292e80200cbc9fc028cfa6bb612d2df9ea903
LaupWing <loc_strict@hotmail.com>
2019-04-02 21:59:55 +0200

    changes

--- src/routes.js
+++ src/routes.js
@@ -4,28 +4,102 @@ const fetch = require('node-fetch');
 const url = 'http://mirabeau.denniswegereef.nl/api/v1/rooms'
 
 router.get('/', (req,res)=>{
-    getData(url)
-        .then(data => {
-            const cleanData = data.map(room=>normalValues(room))
-            let objectkeys = Object.keys(cleanData[0])
-            const keys = objectkeys
-                                .filter(deleteItemFromArray.bind(null,'roomName'))
-                                .filter(deleteItemFromArray.bind(null, 'occupancy'))
-            const keysTitel = keys.map(splitAndCapatalize)
-            const values = cleanData.map((item)=>{return Object.values(item)})
-            console.log(res.render)
+    dataForRendering()
+        .then(data=>{
             res.render('index',{
-                data: cleanData,
-                keys,
-                keysTitel,
-                values
+                data: data.cleanData,
+                keys: data.keys,
+                keysTitel: data.keysTitel,
+                values: data.values,
+                checked: data.checked
+            })
+        })
+})
+router.post('/filter', (req,res)=>{
+    const settings = {
+        filtered: true,
+        info: req.body.info,
+        sortOption: req.body.sortOption,
+        availabilty: req.body.availabilty
+    }
+    dataForRendering(settings)
+        .then(data=>{
+            res.render('index',{
+                data: data.cleanData,
+                keys: data.keys,
+                keysTitel: data.keysTitel,
+                values: data.values,
+                checked: data.checked
             })
         })
 })
 
+function dataForRendering(settings){
+    console.log('settings: ',settings)
+    return getData(url)
+            .then(data => {
+                let cleanData = data.map(room=>normalValues(room))
+                let checked = ''
+                if(settings){
+                    if(settings.availabilty !== 'geen'){
+                        console.log('volgens mij heb ik het')
+                        cleanData = cleanData.filter(filterAvailability(settings.availabilty))
+                    }else{
+                        console.log('None')
+                    }
+                    cleanData.sort(propComparator(settings.info, settings.sortOption))
+                    checked = settings.info
+                }
+                // console.log(cleanData)
+                let objectkeys = Object.keys(cleanData[0])
+                const keys = objectkeys
+                                    .filter(deleteItemFromArray.bind(null,'roomName'))
+                                    .filter(deleteItemFromArray.bind(null, 'occupancy'))
+                const keysTitel = keys.map(splitAndCapatalize)
+                const values = cleanData.map((item)=>{return Object.values(item)})
+                return{
+                    keys,
+                    cleanData,
+                    keysTitel,
+                    values,
+                    checked
+                }
+            })
+}
+
 function splitAndCapatalize(word){
     return (word[0].toUpperCase()+word.slice(1)).match(/[A-Z][a-z]+|[0-9]+/g).join(" ")
 }
+
+function filterAvailability(condition){
+    return function(element){
+
+        // if(condition === 'false'){
+        //     if(element.occupancy === false){
+        //         return element
+        //     }
+        // }else{
+        //     if(element.occupancy === true){
+        //         return element
+        //     }
+        // }
+        // Dit hieronder werkt niet??? (als element.occupancy(true) gelijk is aan condtion(true))
+        // console.log(conditio.toString())
+        const bool = element.occupancy.toString()
+        return bool === condition
+    }
+}
+
+function propComparator(prop, order) {
+    return function(a, b) {
+        if(order === 'Low'){
+            return a[prop] - b[prop];
+        }else{
+            return b[prop] - a[prop];
+        }
+    }
+}
+
 
 function normalValues(object){
     return {
@@ -52,4 +126,4 @@ function getData(url){
 }
 
 
-module.exports = router;
+module.exports = router
